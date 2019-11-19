481ce46f3c7923faa6e6512f76e626571484148c
Juliya Potapchuk <potapchuk.julya@list.ru>
2019-11-19 18:05:30 +0300

    refactor: add one style for quotes/change names for variables

--- codejam-canvas/main.js
+++ codejam-canvas/main.js
@@ -1,18 +1,18 @@
-const button1 = document.getElementById("button1");
-const button3 = document.getElementById("button3");
+const buttonDrawSize4 = document.getElementById('buttonDrawSize4');
+const buttonDrawSize32 = document.getElementById('buttonDrawSize32');
 
-button1.addEventListener('click', function () {
-    drawCanvas1(picture1);
+buttonDrawSize4.addEventListener('click', function() {
+    drawCanvasSize4(picture1);
 });
 
-let picture1 = [["00BCD4", "FFEB3B","FFEB3B","00BCD4"],
-               ["FFEB3B", "FFC107","FFC107","FFEB3B"],
-               ["FFEB3B", "FFC107","FFC107","FFEB3B"],
-               ["00BCD4", "FFEB3B","FFEB3B","00BCD4"]];
+let picture1 = [['00BCD4', 'FFEB3B','FFEB3B','00BCD4'],
+               ['FFEB3B', 'FFC107','FFC107','FFEB3B'],
+               ['FFEB3B', 'FFC107','FFC107','FFEB3B'],
+               ['00BCD4', 'FFEB3B','FFEB3B','00BCD4']];
 
-function drawCanvas1(picture) {
-    const canvas = document.querySelector("canvas");
-        ctx = canvas.getContext("2d");
+function drawCanvasSize4(picture) {
+    const canvas = document.querySelector('canvas');
+        ctx = canvas.getContext('2d');
         width = picture[0].length;
         height = picture.length;
         scale = 512 / picture.length;
@@ -22,18 +22,18 @@ function drawCanvas1(picture) {
 
     for (let row = 0; row < height; row++) {
         for (let col = 0; col < width; col++) {
-            ctx.fillStyle = "#" + picture[row][col];
+            ctx.fillStyle = '#' + picture[row][col];
             ctx.fillRect(col * scale, row * scale, scale, scale);
         }
     }
 }
 
-button3.addEventListener('click', function (){
-    const canvas = document.getElementById("canvas");
+buttonDrawSize32.addEventListener('click', function(){
+    const canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
     pic = new Image();
     pic.src = './images/icon_rsschool.png';
     pic.onload = function() {
-    ctx.drawImage(pic,0,0,512,512);
+    ctx.drawImage(pic, 0, 0, 512, 512);
 }
 });
