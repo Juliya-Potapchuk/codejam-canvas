3be665f7d2f6adcda84f12cf49d3989ac03b22c7
Juliya Potapchuk <potapchuk.julya@list.ru>
2019-11-20 21:18:43 +0300

    fix: change non correct name buttonDrawSize256/structure buttonDrawSize4

--- codejam-canvas/main.js
+++ codejam-canvas/main.js
@@ -1,21 +1,17 @@
 const buttonDrawSize4 = document.getElementById('buttonDrawSize4');
-const buttonDrawSize32 = document.getElementById('buttonDrawSize32');
+const buttonDrawSize256 = document.getElementById('buttonDrawSize256');
 
-buttonDrawSize4.addEventListener('click', function() {
-    drawCanvasSize4(picture1);
-});
-
-let picture1 = [['00BCD4', 'FFEB3B','FFEB3B','00BCD4'],
+let picture = [['00BCD4', 'FFEB3B','FFEB3B','00BCD4'],
                ['FFEB3B', 'FFC107','FFC107','FFEB3B'],
                ['FFEB3B', 'FFC107','FFC107','FFEB3B'],
                ['00BCD4', 'FFEB3B','FFEB3B','00BCD4']];
 
-function drawCanvasSize4(picture) {
+buttonDrawSize4.addEventListener('click', function() {
     const canvas = document.querySelector('canvas');
-        ctx = canvas.getContext('2d');
-        width = picture[0].length;
-        height = picture.length;
-        scale = 512 / picture.length;
+    ctx = canvas.getContext('2d');
+    width = picture[0].length;
+    height = picture.length;
+    scale = 512 / picture.length;
 
     canvas.width = width * scale;
     canvas.height = height * scale;
@@ -24,16 +20,16 @@ function drawCanvasSize4(picture) {
         for (let col = 0; col < width; col++) {
             ctx.fillStyle = '#' + picture[row][col];
             ctx.fillRect(col * scale, row * scale, scale, scale);
-        }
-    }
-}
+        };
+    };
+});
 
-buttonDrawSize32.addEventListener('click', function(){
+buttonDrawSize256.addEventListener('click', function(){
     const canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
     pic = new Image();
     pic.src = './images/icon_rsschool.png';
     pic.onload = function() {
-    ctx.drawImage(pic, 0, 0, 512, 512);
-}
+        ctx.drawImage(pic, 0, 0, 512, 512);
+    };
 });
